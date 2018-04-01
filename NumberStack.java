2d10a67d30ddec61cf1f7a0afa04ffef4772c603
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-01 22:07:08 +0700

    pf- java-oop number stack

--- NumberStack.java
+++ NumberStack.java
@@ -1,2 +1,29 @@
 public class NumberStack {
+    int count = 0;
+    //int top = 0;
+    int[] numbers = new int[20];
+
+    public NumberStack() {
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void push(int number) {
+        numbers[count] = number;
+        count++;
+    }
+    public int pop(){
+        int top = numbers[count - 1];
+        count--;
+        return top;
+    }
+    public int peak() {
+        return 0;
+    }
+
+    public void empty() {
+
+    }
 }
--- NumberStackTest.java
+++ NumberStackTest.java
@@ -1,5 +1,33 @@
 public class NumberStackTest {
-    public static void main(String[] args) {
-
+    public static  void testthat (boolean condition){
+        if(condition){
+            System.out.println("suscess");
+        }else{
+            System.out.println("false");
+        }
     }
+    public static void main(String[] args) {
+        NumberStack numberStack = new NumberStack();
+
+        testthat(numberStack.getCount()==0);
+
+        numberStack.push(10);
+        testthat(numberStack.getCount()==1);
+
+        numberStack.push(20);
+        testthat(numberStack.getCount()==2);
+
+        testthat(numberStack.pop()==20);
+        testthat(numberStack.pop()==10);
+
+        numberStack.push(100);
+        numberStack.push(200);
+        testthat(numberStack.peak() == 200);
+        testthat(numberStack.peak() == 200);
+        testthat (numberStack.getCount() == 2);
+
+        numberStack.empty();
+        testthat(numberStack.getCount() == 0);
+    }
+
 }
